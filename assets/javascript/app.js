c75f73d793ff527258533241eb911d4f359e87ba
JBrozinski <joseph.brozinski@gmail.com>
2020-03-01 18:22:27 -0700

    created first variables and functions.

---
+++ assets/javascript/app.js
@@ -0,0 +1,97 @@
+// Trivia Game
+console.log(questions);
+
+// variables - start button
+//             questions and answer Array
+//             correct, incorrect
+//             Timer
+//             done button
+//             score card
+var startScreen = $("#startScreen"),
+  gameScreen = $("#gameScreen"),
+  scoreScreen = $("#scoreScreen");
+
+var timerEl = $("#timer"),
+  incorrectEl = $("#incorrect"),
+  correctEl = $("#correct"),
+  questionEl = $("#question"),
+  choicesEl = $("#choices");
+
+var startBtn = $("#startBtn"),
+  submitBtn = $("#submitBtn");
+
+var timerLeft, correct, incorrect, intervalId, qIndex;
+
+startBtn.on("click", startGame);
+submitBtn.on("click", checkAnswer);
+
+function startGame() {
+  timerLeft = 30;
+  correct = 0;
+  incorrect = 0;
+  qIndex = 0;
+
+  gameScreen.show();
+  startScreen.hide();
+  scoreScreen.hide();
+
+  displayQuestion(questions[qIndex]);
+  startTimer();
+}
+
+// var questionObj = value passed to function call
+// var questionObj === questions[qIndex]
+function displayQuestion(questionObj) {
+  questionEl.text(questionObj.question);
+  for (var i = 0; i < questionObj.answers.length; i++) {
+    var div = $("<div>");
+
+    var choice = $("<input type='radio' name='hello' >");
+    choice.val(questionObj.answers[i]);
+
+    var text = $("<span>");
+    text.text(questionObj.answers[i]);
+    text.addClass("ml-2");
+
+    div.append(choice, text);
+    choicesEl.append(div);
+  }
+}
+
+function checkAnswer() {
+  console.log("Check answer", questions[qIndex].correct)
+  console.log($("input[name='choice']:checked").val())
+}
+
+function endGame() {
+  gameScreen.hide();
+  startScreen.hide();
+  scoreScreen.show();
+
+  correctEl.text(correct);
+  incorrectEl.text(incorrect);
+}
+
+function startTimer() {
+  clearInterval(intervalId);
+  intervalId = setInterval(function() {
+    timerLeft--;
+    timerEl.text(timerLeft);
+
+    if (timerLeft === 0) {
+      clearInterval(intervalId);
+
+      endGame();
+    }
+  }, 1 * 1000);
+}
+// functions -     populate questions and answers
+//                 start Game and Timer
+// //                 timer stops Game
+// //                 score card appears and shows score
+
+//     steps  -        press start
+//                     questions generate, timer starts
+//                     user clicks answers, one per question
+//                     game ends when timer runs out or user clicks "done"
+//                     score card shows "correct" "incorrect" and "unanswered"
---
+++ assets/javascript/questions.js
@@ -0,0 +1,12 @@
+var questions = [
+  {
+    question: "Pick B",
+    answers: ["A", "B", "C"],
+    correct: "B"
+  },
+  {
+    question: "Pick 3",
+    answers: ["1", "2", "3"],
+    correct: "3"
+  }
+];
